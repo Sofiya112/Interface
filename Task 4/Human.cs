b384efe729bd32313a1899926e7f1b31526409d6
Sofiya112 <sofiya032005@gmail.com>
2025-04-17 11:09:06 +0300

    Add files via upload

---
+++ Task 4/Human.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Human :  Human2
+    {
+        public void Method1()
+        {
+            Console.WriteLine(" Говорит громко - Я люблю программирование !");
+        }
+        public void Method2()
+        {
+            Console.WriteLine(" Думает - А С# больше всего");
+        }
+        public void Method3()
+        {
+            Console.WriteLine(" Говорит нормально - Меньше люблю компьютерные игры !");
+        }
+    }
+}
---
+++ Task 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human();
+            human.Method1();
+            human.Method2();
+            human.Method3();
+
+            Console.ReadLine();
+        }
+    }
+}
